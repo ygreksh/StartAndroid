56fb2eb61e48173be1b2e8e23799fd69c464a192
ygrek <0ubhjrGIT>
2018-08-01 09:08:24 +0300

    P0102_ActivityListener

---
+++ p0102activitylistener/src/main/java/ru/startandroid/p0102activitylistener/MainActivity.java
@@ -0,0 +1,31 @@
+package ru.startandroid.p0102activitylistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOut = findViewById(R.id.tvOut);
+        btnOk = findViewById(R.id.btnOk);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnOk: tvOut.setText("Нажата кнопка ОК"); break;
+            case R.id.btnCancel: tvOut.setText("Нажата кнопка Cancel");break;
+        }
+    }
+}
