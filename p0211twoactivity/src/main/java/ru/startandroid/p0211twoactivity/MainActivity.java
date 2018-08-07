c32a3cc964dbf2ac95b2be083c54c2e4cf69aa1e
ygreksh <31619646+ygreksh@users.noreply.github.com>
2018-08-08 01:10:43 +0300

    p0211twoactivity

---
+++ p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/MainActivity.java
@@ -0,0 +1,31 @@
+package ru.startandroid.p0211twoactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnActTwo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                // TODO Call second activity
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
