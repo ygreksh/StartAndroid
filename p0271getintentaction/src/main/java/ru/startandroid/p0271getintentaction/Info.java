2887fc7a2014db98cab516a347e986ed7c473473
ygreksh <31619646+ygreksh@users.noreply.github.com>
2018-08-14 10:27:16 +0300

    P0281_IntentExtras

---
+++ p0271getintentaction/src/main/java/ru/startandroid/p0271getintentaction/Info.java
@@ -0,0 +1,43 @@
+package ru.startandroid.p0271getintentaction;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Info extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+
+        // получаем Intent, который вызывал это Activity
+        Intent intent = getIntent();
+        // читаем из него action
+        String action = intent.getAction();
+
+        String format = "", textInfo = "";
+
+        // в зависимости от action заполняем переменные
+        if (action.equals("ru.startandroid.intent.action.showtime")) {
+            format = "HH:mm:ss";
+            textInfo = "Time: ";
+        }
+        else if (action.equals("ru.startandroid.intent.action.showdate")) {
+            format = "dd.MM.yyyy";
+            textInfo = "Date: ";
+        }
+
+        // в зависимости от содержимого переменной format
+        // получаем дату или время в переменную datetime
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String datetime = sdf.format(new Date(System.currentTimeMillis()));
+
+        TextView tvDate = (TextView) findViewById(R.id.tvInfo);
+        tvDate.setText(textInfo + datetime);
+    }
+}
